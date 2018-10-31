a2c34e69e7489564e8daf3999b929b7755891d7d
Leyla <cheleyla@gmail.com>
2018-10-31 10:03:19 -0300

    Se crean los ambs de empleados y profesores

--- src/main/java/ar/edu/uade/inte/gym/dao/ProfesorController.java
+++ src/main/java/ar/edu/uade/inte/gym/dao/ProfesorController.java
@@ -1,6 +1,8 @@
 package ar.edu.uade.inte.gym.dao;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import ar.edu.uade.inte.gym.bean.Profesor;
 
@@ -8,10 +10,17 @@ import ar.edu.uade.inte.gym.bean.Profesor;
 public class ProfesorController extends EntityController<Profesor>{
 
 	private static final long serialVersionUID = 5570046968160556665L;
+	
+    @PersistenceContext
+    private EntityManager em;
 
 	@Override
 	public Class<Profesor> getEntityClass() {
 		return Profesor.class;
 	}
+	
+	
+	
+	
 	
 }
